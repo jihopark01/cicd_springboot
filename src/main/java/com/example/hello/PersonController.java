746889e5cb3d76679279f8492d86dfbf36f4d8a4
jihopark01 <jihop3355@gmail.com>
2023-01-04 22:28:29 +0900

    build test

---
+++ src/main/java/com/example/hello/PersonController.java
@@ -0,0 +1,18 @@
+package com.example.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @GetMapping("/myinfo")
+    public Person getPerson() {
+        Person person = new Person();
+        person.setName("HDH");
+        person.setAge(25);
+        person.setAddress("경기");
+        person.setJob("대학생");
+        return person;
+    }
+}
